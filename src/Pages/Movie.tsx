649d8953deb64a4b520cb3a16c5829517005242a
Naufal Yukafi <naufalyukafi10@gmail.com>
2021-01-14 20:47:41 +0700

    feat loding movie

--- src/Pages/Movie.tsx
+++ src/Pages/Movie.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Typography, Input, Layout } from "antd"
+import { Typography, Input, Layout, Spin, Col } from "antd"
 import { Link } from "react-router-dom"
 import axios from "axios";
 import { API_KEY, BASE_URL } from "../store";
@@ -8,11 +8,13 @@ import MovieList from "../components/MovieList"
 
 const Movie = () => {
     const [movie, setMovie] = React.useState<IMovieListData>();
-    const [data, setData] = React.useState("")
+    const [data, setData] = React.useState("");
+    const [loading, setLoading] = React.useState(true);
     const fetchMovie = async () => {
         try {
             const result = await axios(`${BASE_URL}/discover/movie?api_key=${API_KEY}`)
             setMovie(result.data)
+            setLoading(false)
         } catch (error) {
             console.log(error)
         }
@@ -33,41 +35,46 @@ const Movie = () => {
 
     return (
         <Layout className="container" style={{ backgroundColor: "#222" }}>
-            <Typography.Title level={3} style={{ textAlign: 'center', marginTop: 10, color: "#fff" }} >Movie Catalogue</Typography.Title>
-            <Input.Search
-                allowClear
-                enterButton
-                placeholder="Search movie in here ..."
-                onChange={handleChange}
-                value={data}
-            />
-            <Layout.Content>
-                {data.length > 0 ?
-                    filteredData?.map(item => (
-                        <Link to={`/detail/${item.id}`}>
-                            <MovieList
-                                key={item.id}
-                                id={item.id}
-                                backdrop_path={item.backdrop_path}
-                                original_title={item.original_title}
-                                release_date={item.release_date}
-                                vote_average={item.vote_average}
-                            />
-                        </Link>
-                    )) : movie?.results.map(item => (
-                        <Link to={`/detail/${item.id}`}>
-                            <MovieList
-                                key={item.id}
-                                id={item.id}
-                                backdrop_path={item.backdrop_path}
-                                original_title={item.original_title}
-                                release_date={item.release_date}
-                                vote_average={item.vote_average}
-                            />
-                        </Link>
-                    ))}
+            {loading ?
+                <Col style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "65vh" }}>
+                    <Spin size="large" tip="Loading movie..." />
+                </Col>
+                : <>
+                    <Typography.Title level={3} style={{ textAlign: 'center', marginTop: 10, color: "#fff" }} >Movie Catalogue</Typography.Title>
+                    <Input.Search
+                        allowClear
+                        enterButton
+                        placeholder="Search movie in here ..."
+                        onChange={handleChange}
+                        value={data}
+                    />
+                    <Layout.Content>
+                        {data.length > 0 ?
+                            filteredData?.map(item => (
+                                <Link to={`/detail/${item.id}`}>
+                                    <MovieList
+                                        key={item.id}
+                                        id={item.id}
+                                        backdrop_path={item.backdrop_path}
+                                        original_title={item.original_title}
+                                        release_date={item.release_date}
+                                        vote_average={item.vote_average}
+                                    />
+                                </Link>
+                            )) : movie?.results.map(item => (
+                                <Link to={`/detail/${item.id}`}>
+                                    <MovieList
+                                        key={item.id}
+                                        id={item.id}
+                                        backdrop_path={item.backdrop_path}
+                                        original_title={item.original_title}
+                                        release_date={item.release_date}
+                                        vote_average={item.vote_average}
+                                    />
+                                </Link>
+                            ))}
 
-            </Layout.Content>
+                    </Layout.Content> </>}
         </Layout>
     )
 }
